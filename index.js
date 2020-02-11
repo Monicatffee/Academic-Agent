bb9d34d1896c6add405793cf521bebf889e79718
Monica Echeverri <monica.e@ideaslab.com.co>
2020-02-10 21:59:17 -0500

    make it better

--- index.js
+++ index.js
@@ -94,8 +94,8 @@ const getNameFromWhatsapp = (req, res) => {
 
 const getAppointment = (req, res) => {
     const phone = req.body.queryResult.parameters['phone-number'];
-    const tipo = req.body.queryResult.outputContexts.parameters[TipoPizza];
-    const tamano = req.body.queryResult.outputContexts.parameters[TamanoPizza]; 
+    const tipo = req.body.queryResult.outputContexts[0].parameters.TipoPizza;
+    const tamano = req.body.queryResult.outputContexts[0].parameters.TamanoPizza; 
     //const tipo = req.body.queryResult.outputContexts.parameters.TipoPizza;
     //const tamano = req.body.queryResult.outputContexts.parameters.TamanoPizza;
     console.log(phone, tipo, tamano)
